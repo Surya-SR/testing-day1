46c804f3a645b3a4d5d34129eb3e8cd24df83e12
Lokesh <lokesh.apr24@gmail.com>
2024-10-23 18:03:06 +0530

    updated lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
 import { PrimeReactProvider } from 'primereact/api';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Counter from './components/Counter';
-import Input from './components/Input';
+// import Counter from './components/Counter';
+// import Input from './components/Input';
 import Medallion from "./page/medallion";
+import Layout from './page/Layout';
+// import DemoPrimeTest from './components/DemoPrimeTest';
 import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
 import "./styles/style.scss";
-import Layout from './page/Layout';
-import DemoPrimeTest from './components/DemoPrimeTest';
 
 function App() {
   return ( 
     <PrimeReactProvider value={{
       ripple: true}}>
-        <Counter></Counter>
-        <Input></Input>
-        <DemoPrimeTest></DemoPrimeTest>
-      {/* <BrowserRouter>
+      <BrowserRouter>
          <Routes>
            <Route path='/' element={<Layout/>} >
            <Route index element={<Medallion/>} ></Route>
            </Route>
          </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
     </PrimeReactProvider>
   );
 }
--- src/page/Layout.jsx
+++ src/page/Layout.jsx
@@ -1,9 +1,19 @@
+import Counter from "../components/Counter";
+import DemoPrimeTest from "../components/DemoPrimeTest";
 import Header from "../components/Header";
+import Input from "../components/Input";
 
 const Layout = ({ children }) => {
   return (
     <main>
+      {/*     "build:dev": "env-cmd -f .env.dev npm run test && env-cmd -f .env.dev npm run build",
+    "build:qa": "env-cmd -f .env.qa npm run test && env-cmd -f .env.qa npm run build",
+    "build:uat": "env-cmd -f .env.uat npm run test && env-cmd -f .env.uat npm run build",
+    "build:prod": "env-cmd -f .env.prod npm run test && env-cmd -f .env.prod npm run build", */}
       <Header />
+      <Counter></Counter>
+        <Input></Input>
+        <DemoPrimeTest></DemoPrimeTest>
       <section>{children}</section>
     </main>
   );
