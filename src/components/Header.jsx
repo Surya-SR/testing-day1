652cbc7c2dd95e6e06c4ce70dd645734cfa36c5c
Lokesh <lokesh.apr24@gmail.com>
2024-10-24 12:41:49 +0530

    eslint updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 const Header = () => {
   return (
-        <header className="d-flex align-items-center justify-content-between">
+        <header className="d-flex align-items-center justify-content-between w-100">
            <a href="/"><img src="/assets/images/react.png" alt="" /></a>
            <div className="">
             Header
+           </div>
+           <div className="">
+            
+
            </div>
         </header>
   )
